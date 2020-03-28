35127f00208c45db6fc28b4bb9a4f51119878f60
alxdx <ustongs09@gmail.com>
2020-03-27 19:58:09 -0600

    reajuste de jerarquia y server agregado

---
+++ static/app.js
@@ -0,0 +1,6 @@
+//esto contine todas las funciones que index.html utiliza
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.autocomplete');
+    var instances = M.Autocomplete.init(elems, options);
+  });
